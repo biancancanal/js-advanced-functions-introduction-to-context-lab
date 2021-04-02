2038a9e138de9358a3dbdb3bddc6eb27f4225c1e
Bianca <biancancanal@gmail.com>
2021-04-02 10:07:00 -0500

    commit tree e6072701573c9f3b72a55ebd858500f322ad866b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+
+    
+
+
+function createEmployeeRecord(employeeRowData) {
+    let object = {
+        firstName: employeeRowData[0], 
+        familyName: employeeRowData[1], 
+        title: employeeRowData[2], 
+        payPerHour: employeeRowData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return object;
+}
+
+function createEmployeeRecords(array) {
+    console.log(array)
+    let records = {
+        firstName: array[0], 
+        familyName: array[1], 
+        title: array[2], 
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return records;
+}
